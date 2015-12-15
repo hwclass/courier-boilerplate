65918bdab78c4c285655f36b356649172a619de1
hwclass <harderworking.class@gmail.com>
2015-12-15 18:07:34 +0200

    removed the unncessary code in index router

--- routes/index.js
+++ routes/index.js
@@ -16,23 +16,3 @@ module.exports = {
 	}
   }
 };
-
-'use strict';
-
-module.exports = {
-  method: 'GET',
-  path: '/user/{userId}',
-  handler: function(request, reply) {
-    var data = {
-      title : 'User View',
-      user : request.params.userId
-    };
-    return reply.view('user', data);
-  },
-  config : {
-  	state: {
-        parse: false, // parse and store in request.state
-		failAction: 'ignore' // may also be 'ignore' or 'log'
-	}
-  }
-};
