75d79dcc0455dd383be4366744d0959f8c130a63
hwclass <harderworking.class@gmail.com>
2016-04-04 23:25:10 +0300

    changed the indentation of dummyText modules

--- client/src/apps/header/services/dummyText.js
+++ client/src/apps/header/services/dummyText.js
@@ -2,7 +2,7 @@ App.header.addService('dummyText', function (context) {
   'use strict';
   return {
     getDummyText : function () {
-      return 'This is a dummy text..';
+    return 'This is a dummy text..';
     }
   }
 });
