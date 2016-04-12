bca61347cab74a8ffd20e95d30c3d8be668f9d36
hwclass <harderworking.class@gmail.com>
2016-04-12 23:34:36 +0300

    edited the indentation in router index file

--- routes/all.js
+++ routes/all.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const indexRoute = require(path.join(__dirname, 'index')),
-	    aboutRoute = require(path.join(__dirname, 'about')),
+      aboutRoute = require(path.join(__dirname, 'about')),
       userRoute = require(path.join(__dirname, 'user'));
 
 module.exports = [indexRoute, aboutRoute, userRoute];
